c2d79d26259a41dc0805808568a939bd9f2263e9
NeutronCL <jbecofla@gmail.com>
2022-05-16 22:04:37 -0400

    validations done

---
+++ assets/js/main.js
@@ -0,0 +1,27 @@
+const email = document.getElementById("email");
+console.log(email);
+const errorText = document.getElementById("error-text");
+const form = document.getElementById("hero__subscribe");
+const errorIcon = document.getElementById("error-icon");
+const correctIcon = document.getElementById("correct-icon");
+const emailPattern =
+  /[a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*@[a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*[.][a-zA-Z]{2,5}/;
+
+email.addEventListener("blur", (e) => {
+  if (email.value.match(emailPattern)) {
+    console.log("correo correcto");
+    form.classList.add("form-correct");
+    form.classList.remove("form-error");
+    errorIcon.classList.add("hidden");
+    correctIcon.classList.remove("hidden");
+    errorText.textContent = "";
+  } else {
+    e.preventDefault();
+    console.log("mala");
+    form.classList.remove("form-correct");
+    form.classList.add("form-error");
+    errorIcon.classList.remove("hidden");
+    correctIcon.classList.add("hidden");
+    errorText.textContent = "Please provide a valid email.";
+  }
+});
